e6d63cf4d92bd39054cfb9ae80f515c2e11425eb
gerhard.nospam <gerhard.nospam@f055d478-af1b-d3f0-76c2-19c3c2adf069>
2010-09-06 18:47:24 +0000

    Set version to major.minor.svnrev.* Bump from 1.4 to 1.5

--- Lofas.SportsTracks.Hitta_SE_MapProvider/Properties/AssemblyInfo.cs
+++ Lofas.SportsTracks.Hitta_SE_MapProvider/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Löfås")]
 [assembly: AssemblyProduct("Hitta.se /Eniro.se Mapprovider plugin")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright ©  2008-2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.3.0")]
-[assembly: AssemblyFileVersion("1.4.3.0")]
+[assembly: AssemblyVersion("1.5.15.*")]
+[assembly: AssemblyFileVersion("1.5")]
