300e4f1684215a33c7dbb9f0c71354861dcaca6c
magwalls@gmail.com <magwalls@gmail.com@f055d478-af1b-d3f0-76c2-19c3c2adf069>
2010-10-06 07:08:30 +0000

    Fix: Problem with that map tiles wasn't updated in ST immediately after download has been improved and maybe solved.

--- Lofas.SportsTracks.Hitta_SE_MapProvider/Common/STWebClient.cs
+++ Lofas.SportsTracks.Hitta_SE_MapProvider/Common/STWebClient.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (C) 2008, 2009, 2010 Peter Löfås
+Copyright (C) 2010 Magnus Wallström
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
