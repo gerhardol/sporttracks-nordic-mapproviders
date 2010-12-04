c9ccf59280c8b7017f9d7fb676f6af6cd3fa5f4c
gerhard.nospam <gerhard.nospam@f055d478-af1b-d3f0-76c2-19c3c2adf069>
2010-12-04 09:24:08 +0000

    * Avoid compiling depreciated Eniro files

--- Lofas.SportsTracks.Hitta_SE_MapProvider/MapProviders/Deprecated/Eniro_SE_MapProvider.cs
+++ Lofas.SportsTracks.Hitta_SE_MapProvider/MapProviders/Deprecated/Eniro_SE_MapProvider.cs
@@ -15,6 +15,8 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#if false
+//No longer used
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -420,3 +422,4 @@ namespace Lofas.SportsTracks.Hitta_SE_MapProvider
 #endif
     }
 }
+#endif
