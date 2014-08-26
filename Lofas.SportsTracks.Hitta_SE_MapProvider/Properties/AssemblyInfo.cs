d9be4c108d462ea043c6b0e7ed3852f9723f8a7f
gerhard.nospam <gerhard.nospam@f055d478-af1b-d3f0-76c2-19c3c2adf069>
2014-08-26 20:58:04 +0000

    Add (brief) changes document, bump version to match svn

--- Lofas.SportsTracks.Hitta_SE_MapProvider/Properties/AssemblyInfo.cs
+++ Lofas.SportsTracks.Hitta_SE_MapProvider/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Löfås")]
 [assembly: AssemblyProduct("Hitta.se /Eniro.se Mapprovider plugin")]
-[assembly: AssemblyCopyright("Copyright ©  2008-2010")]
+[assembly: AssemblyCopyright("Copyright ©  2008-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.44.*")]
+[assembly: AssemblyVersion("1.6.51.*")]
 [assembly: AssemblyFileVersion("1.6")]
